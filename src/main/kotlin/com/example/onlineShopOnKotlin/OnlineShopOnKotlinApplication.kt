5f8afedd228602a1c3d92f4a2a530b69027da7bd
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-11-07 23:18:04 +0600

    first commit

---
+++ src/main/kotlin/com/example/onlineShopOnKotlin/OnlineShopOnKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.onlineShopOnKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineShopOnKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineShopOnKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/onlineShopOnKotlin/OnlineShopOnKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.onlineShopOnKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnlineShopOnKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
