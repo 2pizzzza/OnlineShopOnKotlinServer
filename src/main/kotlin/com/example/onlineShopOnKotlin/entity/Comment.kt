726cf82ecce0edf4cc66ff7c94166166b9626deb
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-11-09 23:19:47 +0600

    added entiti comment

---
+++ src/main/kotlin/com/example/onlineShopOnKotlin/entity/Comment.kt
@@ -0,0 +1,27 @@
+package com.example.onlineShopOnKotlin.entity
+
+import jakarta.persistence.*
+import lombok.AllArgsConstructor
+import lombok.Getter
+import lombok.NoArgsConstructor
+import lombok.Setter
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class Comment(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id:Long,
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    val products: List<Product> = mutableListOf(),
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    val users: List<User> = mutableListOf()
+)
