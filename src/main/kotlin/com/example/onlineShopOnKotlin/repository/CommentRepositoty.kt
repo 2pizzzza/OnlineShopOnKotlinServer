618dc3935b0e8598b673a48f614065ee25438847
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-11-11 20:38:45 +0600

    added comment repositury

--- src/main/kotlin/com/example/onlineShopOnKotlin/repository/CommentRepositoty.kt
+++ src/main/kotlin/com/example/onlineShopOnKotlin/repository/CommentRepositoty.kt
@@ -1,10 +1,9 @@
 package com.example.onlineShopOnKotlin.repository
 
-import com.example.onlineShopOnKotlin.entity.Product
+import com.example.onlineShopOnKotlin.entity.Comment
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductRepository :JpaRepository<Product, Long> {
-    fun findByTitle(name:String): Product
+interface CommentRepositoty: JpaRepository<Comment, Long> {
 }
