fe3abfc12ddeef2bd89a81aaef563b801b5acc08
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-11-19 21:50:36 +0600

    added ProductController

---
+++ src/main/kotlin/com/example/onlineShopOnKotlin/controler/ProductController.kt
@@ -0,0 +1,26 @@
+package com.example.onlineShopOnKotlin.controler
+
+import com.example.onlineShopOnKotlin.entity.Product
+import com.example.onlineShopOnKotlin.service.ProductService
+import lombok.RequiredArgsConstructor
+import org.apache.el.stream.Optional
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+@RequiredArgsConstructor
+class ProductController {
+    private final val productService:ProductService = TODO()
+
+    @GetMapping("allproduct/")
+    fun getAllProduct():List<Product>{
+        return productService.getAllProduct()
+    }
+
+    @GetMapping("getById/{id}")
+    fun getById(id:Long):ResponseEntity<Product>{
+        val product = productService.getProductById(id)
+        return product.map { ResponseEntity.ok(it) }.orElse(ResponseEntity.noContent().build())
+    }
+}
