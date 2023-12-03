be434f09f34a8e05048a0d080012b48de9d350fe
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-12-03 21:28:15 +0600

    added comment controller

---
+++ src/main/kotlin/com/example/onlineShopOnKotlin/controler/CommentControler.kt
@@ -0,0 +1,19 @@
+package com.example.onlineShopOnKotlin.controler
+
+import com.example.onlineShopOnKotlin.entity.Comment
+import com.example.onlineShopOnKotlin.service.CommentService
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequiredArgsConstructor
+class CommentControler {
+
+    val commentService:CommentService = TODO()
+
+    @GetMapping("/comment/all")
+    fun getAllComment():List<Comment>{
+        return  commentService.getAllComment()
+    }
+}
